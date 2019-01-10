d6728a46ef6034739e96c645273c2788942e48be
Sergii Putko <psvmcc@gmail.com>
2019-01-10 16:12:42 +0200

    gcr.io support, also works for k8s.gcr.io. regexp tags option set multiply IMAGENAMES per time

---
+++ gcrio.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"net/url"
+	"os"
+	"path"
+)
+
+const gcrioURLBase = "https://"
+
+type gcrioTag struct {
+	Revision      bool   `json:"revision"`
+	StartTs       int    `json:"start_ts"`
+	Name          string `json:"name"`
+	DockerImageID string `json:"docker_image_id"`
+}
+
+type gcrioTagsResponse struct {
+        Name string   `json:"name"`
+        Tags []string `json:"tags"`
+}
+
+func constructgcrioURL(image string, grepo string) (string, error) {
+	u, err := url.Parse(gcrioURLBase)
+	if err != nil {
+		return "", err
+	}
+
+	u.Path = path.Join(u.Path, grepo, "v2", image, "tags/list")
+	return u.String(), nil
+}
+
+func retriveFromgcrio(image string, grepo string) ([]string, error) {
+	url, err := constructgcrioURL(image, grepo)
+	if err != nil {
+		return nil, err
+	}
+	body, err := httpGet(url, os.Getenv("GCRIO_TOKEN"), nil)
+	if err != nil {
+		return nil, err
+	}
+	var resp gcrioTagsResponse
+
+	if err := json.Unmarshal([]byte(body), &resp); err != nil {
+		return nil, err
+	}
+	tags := resp.Tags
+
+	return tags, nil
+}
--- main.go
+++ main.go
@@ -4,62 +4,96 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"flag"
+	"regexp"
 )
 
 const Usage = `Usage:
-  dockertags IMAGENAME
+  dockertags IMAGENAME ... [IMAGENAME]
 `
 
+
 func main() {
-	if len(os.Args) != 2 {
+	imagePtr := flag.Bool("i", false, "show image path in output")
+	regexPtr := flag.String("r",".+", "regexp tags")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
 		fmt.Fprintln(os.Stderr, Usage)
 		os.Exit(1)
 	}
 
 	var (
 		repo  string
+		grepo string
 		image string
 	)
+	for i := 0; i <= flag.NArg() - 1; i++ {
 
-	ss := strings.Split(os.Args[1], "/")
+		ss := strings.Split(flag.Arg(i), "/")
 
-	if len(ss) > 2 {
-		repo = ss[0]
-		image = strings.Join(ss[1:], "/")
-	} else if len(ss) == 1 {
-		// Official image of DockerHub
-		repo = "hub.docker.com"
-		image = strings.Join(append([]string{"library"}, ss...), "/")
-	} else {
-		repo = "hub.docker.com"
-		image = strings.Join(ss, "/")
-	}
+		if len(ss) > 2 {
+			if strings.Contains(ss[0], "gcr.io") {
+				repo = "gcr.io"
+				grepo = ss[0]
+			} else {
+				repo = ss[0]
+			}
+			image = strings.Join(ss[1:], "/")
+		} else if strings.Contains(ss[0], "gcr.io") {
+			repo = "gcr.io"
+			grepo = ss[0]
+			image = strings.Join(ss[1:], "/")
+		} else if len(ss) == 1 {
+			// Official image of DockerHub
+			repo = "hub.docker.com"
+			image = strings.Join(append([]string{"library"}, ss...), "/")
+		} else {
+			repo = "hub.docker.com"
+			image = strings.Join(ss, "/")
+		}
 
-	var tags []string
+		var tags []string
 
-	switch repo {
-	case "hub.docker.com":
-		t, err := retrieveFromDockerHub(image)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		switch repo {
+		case "hub.docker.com":
+			t, err := retrieveFromDockerHub(image)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			tags = t
+		case "quay.io":
+			t, err := retriveFromQuay(image)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			tags = t
+		case "gcr.io":
+			t, err := retriveFromgcrio(image, grepo)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			tags = t
+		default:
+			fmt.Fprintf(os.Stderr, "Unsupported image repository: %s\n", repo)
 			os.Exit(1)
 		}
 
-		tags = t
-	case "quay.io":
-		t, err := retriveFromQuay(image)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+		for _, tag := range tags {
+		        match, _ := regexp.MatchString(*regexPtr, tag)
+			if match {
+				if flag.NArg() >= 2 || *imagePtr {
+					fmt.Printf("%s:%s\n", flag.Arg(i), tag)
+				} else {
+					fmt.Println(tag)
+				}
+			}
 		}
-
-		tags = t
-	default:
-		fmt.Fprintf(os.Stderr, "Unsupported image repository: %s\n", repo)
-		os.Exit(1)
-	}
-
-	for _, tag := range tags {
-		fmt.Println(tag)
 	}
 }
